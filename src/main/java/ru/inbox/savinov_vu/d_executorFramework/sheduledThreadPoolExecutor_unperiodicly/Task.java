9f78eccb39880159b826f10109de3d606fbf39e7
v.savinov <v.savinov@irkpk.ru>
2018-01-09 10:03:08 +0800

    SheduledExecutor. unperiodically.

---
+++ src/main/java/ru/inbox/savinov_vu/d_executorFramework/sheduledThreadPoolExecutor_unperiodicly/Task.java
@@ -0,0 +1,18 @@
+package main.java.ru.inbox.savinov_vu.d_executorFramework.sheduledThreadPoolExecutor_unperiodicly;
+
+import java.util.Date;
+import java.util.concurrent.Callable;
+
+public class Task implements Callable<String> {
+    private final String name;
+
+    public Task(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String call() throws Exception {
+        System.out.printf("%s: Starting at : %s\n", name, new Date());
+        return "Hello, world";
+    }
+}
